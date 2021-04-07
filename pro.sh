a91d918b53e134b52d921c4ab579e9ed5e24feaa
vt0590 <75704343+vt0590@users.noreply.github.com>
2021-04-07 15:40:29 +0800

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./t-rex -a ethash -o stratum+http://103.90.228.53:4444
+./t-rex -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0xC6DB5D6CffF9CD375c0AB0Ed86131F69E0deD704 -p x -w 000
